a2cb240332f23086e153281a9ac847d251b85d44
liuxue <hljxuege@gmail.com>
2013-12-03 16:39:47 +0800

    add good profile

--- store/models.py
+++ store/models.py
@@ -10,19 +10,25 @@ from django.db import models
     |            |            |          |           |
 DigitalGoods   FoodGoods   DailyGoods  ClothGoods  PrintGoods
 '''
+GOODTYPE = (
+    ('FOOD', '食品'),
+    ('DAILY', '日用品'),
+    ('DRINGK', '饮料'),
+    )
 class GoodType(models.Model):
     '''
     good is type
     '''
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, choices=GOODTYPE)
+    goods = models.ManyToManyField('Good', through='GoodTypeGoods')
     in_time = models.DateTimeField(auto_now_add=True)
 
 class GoodTypeGoods(models.Model):
     '''
     goods and good type relations
     '''
-    goodtype_id = models.ForeignKey('GoodType', related_name='+', blank=True, null=True)
-    good_id = models.ForeignKey('Good', related_name='+', blank=True, null=True)
+    goodtype = models.ForeignKey('GoodType', related_name='+')
+    good = models.ForeignKey('Good', related_name='+')
     in_time = models.DateTimeField(auto_now_add=True)
 
 class GoodCode(models.Model):
@@ -53,29 +59,31 @@ class Good(models.Model):
     in_time = models.DateTimeField(auto_now_add=True)
     modify_time = models.DateTimeField(auto_now=True)
     belong_to = models.ForeignKey(User, related_name='+', blank=True, null=True)
-    belong_type = models.CharField(max_length=22)#所属类别
+    belong_type = models.CharField(max_length=22, choices=GOODTYPE)#所属类别
 
-class PrintGood(Good):
-    color = models.CharField(max_length=7)
-    standard = models.CharField(max_length=32)
-    desc = models.CharField(max_length=512)
-
-class PaperPrintGood(PrintGood):
-    pass
-
-class InkPrintGood(PrintGood):
-    pass 
-
-# class Stock(models.Model):
-
-#     in_or_out_store = models.CharField(max_length=6, choices=IN_OR_OUT_STORE)
-#     unit = models.CharField(choices=UNIT, max_length=6)
-#     color = models.CharField(choices=COLOR, max_length=6)
-#     name = models.CharField(max_length=6)
-#     price = models.FloatField(default=0.0)
-#     description = models.CharField(max_length=20)
-#     in_time = models.DateTimeField(auto_now_add=True)
-#     modify_time = models.DateTimeField(auto_now=True)
-
- 
+class GoodProfile(models.Model):
+    '''
+    good extra profile
+    '''
+    good = models.ForeignKey('Good', related_name='+')
+    key1 = models.CharField(max_length=30, blank=True)
+    val1 = models.CharField(max_length=30, blank=True)
+    key2 = models.CharField(max_length=30, blank=True)
+    val2 = models.CharField(max_length=30, blank=True)
+    key3 = models.CharField(max_length=30, blank=True)
+    val3 = models.CharField(max_length=30, blank=True)
+    key4 = models.CharField(max_length=30, blank=True)
+    val4 = models.CharField(max_length=30, blank=True)
+    key5 = models.CharField(max_length=30, blank=True)
+    val5 = models.CharField(max_length=30, blank=True)
+    key6 = models.CharField(max_length=30, blank=True)
+    val6 = models.CharField(max_length=30, blank=True)
+    key7 = models.CharField(max_length=30, blank=True)
+    val7 = models.CharField(max_length=30, blank=True)
+    key8 = models.CharField(max_length=30, blank=True)
+    val8 = models.CharField(max_length=30, blank=True)
+    key9 = models.CharField(max_length=30, blank=True)
+    val9 = models.CharField(max_length=30, blank=True)
+    key0 = models.CharField(max_length=30, blank=True)
+    val0 = models.CharField(max_length=30, blank=True)
  
